1dd33d0d951eb4c4d4a5775bc31613b6192a7fdf
Steve Barskey <steve@iMac.local>
2017-08-06 15:34:07 -0700

    Initial Commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+	public Transform groundCheck;
+	public LayerMask whatIsGound;
+	public float jumpForce = 200f;
+	public float speed;
+
+	bool facingRight = true;
+	bool grounded = false;
+	float groundRadius = 0.1f;
+
+	private Transform player;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		player = GameObject.Find ("Player").transform;
+	}
+	
+	void FixedUpdate () {
+
+		float move = player.position.x - transform.position.x;
+
+		rb2d.velocity = new Vector2 (move * speed, rb2d.velocity.y);
+
+		if (move > 0 && !facingRight)
+			Flip ();
+		else if (move < 0 && facingRight)
+			Flip ();
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+	public Transform groundCheck;
+	public LayerMask whatIsGound;
+	public float jumpForce = 200f;
+	public float maxSpeed = 5f;
+
+	bool facingRight = true;
+	bool grounded = false;
+	float groundRadius = 0.1f;
+	Animator anim;
+
+	private Rigidbody2D rb2d;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+
+	}
+
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGound);
+
+		float move = Input.GetAxis ("Horizontal");
+
+		anim.SetFloat ("Speed", Mathf.Abs(move));
+
+		rb2d.velocity = new Vector2 (move * maxSpeed, rb2d.velocity.y);
+		anim.SetFloat ("vSpeed", rb2d.velocity.y);
+
+		if (move > 0 && !facingRight)
+			Flip ();
+		else if (move < 0 && facingRight)
+			Flip ();
+	}
+
+	void Update()
+	{
+		if (grounded && Input.GetKeyDown (KeyCode.Space))
+		{
+			// set ground bool in anim
+			rb2d.AddForce(new Vector2(0, jumpForce));
+		}
+
+		if (Input.GetMouseButtonDown(0))
+			Attack();
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	void Attack()
+	{
+		anim.SetTrigger ("Slash");
+	}
+
+}
