923842bd852322a5f507d1f3251bccacdaa2fa62
Steve Barskey <steve@iMac.local>
2017-08-07 00:29:50 -0700

    Daily commit.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-	public Transform groundCheck;
+	public Transform groundCheckCircle;
 	public LayerMask whatIsGound;
 	public float jumpForce = 200f;
 	public float speed;
 	public float speedVar;
-	public LayerMask whatIsPlatform;
 
 	bool facingRight = true;
 	bool grounded = false;
-	float groundRadius = 0.1f;
+	float groundRadius = 0.05f;
+	float jumpHeight = 0.35f;
 
 	private Transform player;
 	private Rigidbody2D rb2d;
@@ -26,36 +26,23 @@ public class EnemyController : MonoBehaviour
 	
 	void FixedUpdate ()
 	{
-		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGound);
+		grounded = Physics2D.OverlapCircle (groundCheckCircle.position, groundRadius, whatIsGound);
 
-		float move = player.position.x - transform.position.x;
+		var playerVector = player.position - transform.position; // vector to the player
 
-		rb2d.velocity = new Vector2 (move * speed, rb2d.velocity.y);
+		rb2d.velocity = new Vector2 (playerVector.x * speed, rb2d.velocity.y);
 
-		if (move > 0 && !facingRight)
+		if (playerVector.x > 0 && !facingRight)
 			Flip ();
-		else if (move < 0 && facingRight)
+		else if (playerVector.x < 0 && facingRight)
 			Flip ();
-	}
 
-	void Update()
-	{
-		float dist_y = player.position.y - transform.position.y;
-		if (grounded && dist_y > 0.1f)
-		{
-			if (PlatformAbove ())
-			{
-				rb2d.AddForce (new Vector2 (0, jumpForce));
-				Debug.Log ("Jumped");
+		if (playerVector.y > jumpHeight) { // if enemy is close in x and player is at least a platform above
+			RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.up, 0.2f, whatIsGound);
+			if (grounded && hit) {
+				rb2d.AddForce (new Vector2 (0f, jumpForce));
 			}
 		}
-
-	}
-
-	bool PlatformAbove()
-	{
-		RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.up, 0.2f, whatIsPlatform);
-		return hit;
 	}
 
 	void Flip()
